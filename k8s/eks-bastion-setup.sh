73cd59358b5d5aa23db6f43c0d6161fe11e894cd
Churious <137260386+Churious@users.noreply.github.com>
2026-01-11 20:26:26 +0900

    Update eks-bastion-setup.sh

--- k8s/eks-bastion-setup.sh
+++ k8s/eks-bastion-setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-curl -fsSL https://raw.githubusercontent.com/Churious/Script/refs/heads/main/k8s/install-eksctl.sh | bash && echo 'eksctl installed'
-curl -fsSL https://raw.githubusercontent.com/Churious/Script/refs/heads/main/k8s/install-kubectl.sh | bash && echo 'kubectl installed'
-curl -fsSL https://raw.githubusercontent.com/Churious/Script/refs/heads/main/k8s/install-k9s.sh | bash && echo 'k9s installed'
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash && echo 'helm installed'
+curl -fsSL https://raw.githubusercontent.com/Churious/Script/refs/heads/main/k8s/install-eksctl.sh | bash
+curl -fsSL https://raw.githubusercontent.com/Churious/Script/refs/heads/main/k8s/install-kubectl.sh | bash
+curl -fsSL https://raw.githubusercontent.com/Churious/Script/refs/heads/main/k8s/install-k9s.sh | bash
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
