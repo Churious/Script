9d0baf0fecccd52f97b474d03f48ddb57862146a
Churious <137260386+Churious@users.noreply.github.com>
2026-01-11 20:23:25 +0900

    Create eks-bastion-setup.sh

---
+++ k8s/eks-bastion-setup.sh
@@ -0,0 +1,4 @@
+curl -fsSL https://raw.githubusercontent.com/Churious/Script/refs/heads/main/k8s/install-eksctl.sh | bash
+curl -fsSL https://raw.githubusercontent.com/Churious/Script/refs/heads/main/k8s/install-kubectl.sh | bash
+curl -fsSL https://raw.githubusercontent.com/Churious/Script/refs/heads/main/k8s/install-k9s.sh | bash
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
